dc40a9a7d9dae9bddf313cdae91c40dab51f8e58
maikellin <31967699+maikellin@users.noreply.github.com>
2017-12-13 16:13:24 -0500

    chap7 hw+

--- chap7 1.py
+++ chap7 1.py
@@ -1,7 +1,29 @@
 mylist=[0,2,3,7,11,12]
 def count_odd(numlist):
-    cant=0
+    count=0
     for i in numlist:
         if i%2 !=0:
-            cant+=1
+            count+=1
+    return count
+
+def sum_even(numlist):
+    sum=0
+    for i in numlist:
+        if i%2 ==0:
+            sum+=i
+    return sum
+
+thelist=[-2,-5,6,-21,100]
+def sum_neg(numlist):
+    sum=0
+    for i in numlist:
+        if i%2 ==0:
+            sum=i
+    return sum
+
+print(count_odd(mylist))
+print(sum_even(mylist))
+print(sum_neg(mylist))
+
+
             
