f55daba3f44b2fe0341bf4d470e9a799a83a8c87
Maikel Lin <31967699+maikellin@users.noreply.github.com>
2018-01-30 21:46:28 -0500

    chap8 hw1

---
+++ chap8.hw.py
@@ -0,0 +1,23 @@
+#name="maikel"
+#newname=name.capitalize()
+#print(newname)
+
+
+#"Python"[1]
+#"Strings are sequences of characters."[5]
+#len("wonderful")
+#"Mystery"[:4]
+#"p" in "Pineapple"
+#"apple" in "Pineapple"
+#"pear" not in "Pineapple"
+#"apple" > "pineapple"
+#"pineapple" < "Peach"
+#nothing happens when i run it
+
+
+prefixes = "JKLMNOPQ"
+suffix = "ack"
+
+for letter in prefixes:
+    while letter == prefixes [5] or letter == prefixes[7]:
+        print(letter + "u" + suffix)
